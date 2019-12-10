8b12081ab91e525504ebb81d146ff66452962bc6
dxre-v2 <allenavriana1816@gmail.com>
2019-12-10 10:46:20 -0600

    R and Article

--- data/ufos/pretty_graphs.R
+++ data/ufos/pretty_graphs.R
@@ -1,8 +1,4 @@
 ufos %>% 
-  # count(
-  #   duration..hours.min.
-  # ) %>% 
-  # arrange(desc(n))
   filter(
   duration..hours.min. == "5 minutes",
   shape == "light" | 
@@ -12,9 +8,7 @@ ufos %>%
     shape == "sphere",
   year >= 1990
   ) %>% 
-  # count(state) %>% arrange(desc(n))
   ggplot(aes(year)) +
   geom_bar(aes(fill = shape), alpha = 0.95) +
   theme_minimal() +
   scale_fill_brewer(type = "qual")
-glimpse(ufos)
